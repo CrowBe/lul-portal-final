5dbb61f6993a7e8cead8f5ee4a9b059d22a91703
Ben Crow <benny_crow91@hotmail.com>
2020-03-20 14:47:12 +0900

    node_modules bug

--- api/server.js
+++ api/server.js
@@ -1,22 +1,43 @@
 import express from 'express';
 import cors from 'cors';
-import router from './routes';
+import jwt from 'express-jwt';
+import jwksRsa from 'jwks-rsa';
+import dotenv from 'dotenv';
+dotenv.config();
 
+// Create a new Express app
 const app = express();
 
-app.use(cors());
+// Accept cross-origin requests from the frontend app
+app.use(cors({ origin: 'http://localhost:3000' }));
 
-app.use(express.json())
+// Set up Auth0 configuration
+const authConfig = {
+  domain: "lulsite.au.auth0.com",
+  audience: "https://api.lookinguplandscapes.com.au"
+};
 
-app.use(router);
+// Define middleware that validates incoming bearer tokens
+// using JWKS from lulsite.au.auth0.com
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
 
-app.get('/', (req, res) => {
-  console.log(req);
-  res.send('Hello from App Engine!');
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithm: ["RS256"]
 });
 
-// Listen to the App Engine-specified port, or 3001 otherwise
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
+// Define an endpoint that must be called with an access token
+app.get("/api/external", checkJwt, (req, res) => {
+  res.send({
+    msg: "Your Access Token was successfully validated!"
+  });
 });
+
+// Start the app
+app.listen(3001, () => console.log('API listening on 3001'));
